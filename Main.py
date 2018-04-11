e3c1afff55165793cd777d4f49ab0476d5de00a0
Tomasz Gaweł <tomekgawel95@gmail.com>
2018-04-11 12:25:08 +0200

    Changes in neural network

--- Main.py
+++ Main.py
@@ -1,21 +1,26 @@
 import DataNormalization
 import DataController
 import NeuralNetwork
-import neurolab as nl
-import numpy as np
 
+# loading data
 columns_names = []
 data_list = []
 DataController.load_data_list("data/results.csv", data_list, columns_names)
 data_list = DataController.clear_outliners(data_list,columns_names)
 quantity_of_columns = len(columns_names)
 
+# copying data for input
+data_input = []
+for row in data_list:
+    data_input.append(list(row))
+
+# normalization of data
 data = DataNormalization.Data(quantity_of_columns)
 data.calc_avg(data_list)
 data.calc_deviation(data_list)
 data.normalize(data_list)
 
-
+# splitting data into input and output list
 input_temp = []
 temp = []
 output_temp = []
@@ -30,7 +35,32 @@ for row in data_list:
     input_temp.append(temp)
     temp = []
 
+# creating neural network
 NN = NeuralNetwork.NeuralNet(input_temp,output_temp)
 NN.create_and_train_nn(quantity_of_columns)
-out = NN.test([[input(), input()]])
-print(out)
+
+# input for the user
+print("Enter data [%]:")
+test_input = []
+i = 0
+for item in columns_names:
+    if i+1 == len(columns_names):
+        test_input.append(float(1))
+        break
+    print(item+": ")
+    x = input()
+    test_input.append(float(x))
+    i += 1
+
+# adding user input into a list with data
+data_input.append(test_input)
+
+# normalization of data with user input
+data2 = DataNormalization.Data(quantity_of_columns)
+data2.calc_avg(data_input)
+data2.calc_deviation(data_input)
+data2.normalize(data_input)
+
+# testing user input
+out = NN.test([data_input[len(data_input)-1][:-1]])
+print(str(round(out[0][0], 2)*100)+"%")
--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -3,19 +3,24 @@ import numpy as np
 
 
 class NeuralNet(object):
-    def __init__(self,input,output):
+    # creating numpy array for input and output
+    def __init__(self, input, output):
         self.input = np.array(input)
         self.output = np.array(output)
         self.net = None
 
+    # creating neural network with w 3 layers using neurolab library
+    # quantity of columns - 1 neurons in input layer, 6 in hidden layer and 1 in output layer
+    # training neural network
     def create_and_train_nn(self, columns):
         i = 0
         pr = []
         while i < columns - 1:
-            pr.append([-5, 5])
+            pr.append([-2, 2])
             i += 1
         self.net = nl.net.newff(pr, [columns-1, 6, 1])
         err = self.net.train(self.input, self.output, show=1)
 
+    # method that returns output of the network using user's input
     def test(self, test_arr):
         return self.net.sim(test_arr)
